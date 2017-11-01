9d3e2454845f367c8c3208fe8a8eea7ef91d3eac
Joe Hill <joe@jkh3.com>
2017-10-31 23:46:44 -0400

    First Augmented Reality App that tracks surface points in the world around it

---
+++ worldTracking/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  worldTracking
+//
+//  Created by BishopHill on 10/31/17.
+//  Copyright © 2017 Dumb Unicorn. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
